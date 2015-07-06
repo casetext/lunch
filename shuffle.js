faeed2888b081e17e9e8919406518bc74268903a
Josh Dague <daguej@email.uc.edu>
2015-07-06 11:17:40 -0700

    spit out group numbers

--- shuffle.js
+++ shuffle.js
@@ -1,15 +1,24 @@
 var ppl = require('./people.js'),
 	_ = require('lodash');
 
-var grp = Math.ceil(ppl.length / 2);
+var grp = Math.ceil(ppl.length / 2), gid = 0;
 
 ppl = _.shuffle(ppl);
 
 process.stdout.write('\n');
+group();
 
 for (var i = 0; i < ppl.length; i++) {
 	process.stdout.write(ppl[i] + ' ');
-	if ((i+1) % grp == 0) process.stdout.write('\n\n');
+	if ((i+1) % grp == 0 && i+1 < ppl.length) {
+		process.stdout.write('\n\n');
+		group();
+	}
 }
 
 process.stdout.write('\n');
+
+
+function group() {
+	process.stdout.write(++gid + '. ');
+}
