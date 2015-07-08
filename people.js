10adad4dcfe3470c5676f0af5e29921e6cac484f
Sarah Jabon <sjabon@gmail.com>
2015-07-08 11:55:08 -0700

    add people.js

---
+++ people.js
@@ -0,0 +1,19 @@
+// comment people out who aren't here today
+module.exports = [
+	"Anand",
+	"Bernardo",
+	"Harry",
+	"Jake",
+	"Josh",
+	"Katherine",
+	"Laura",
+	"Mei",
+	"Nicole",
+	"Pablo",
+	"Richard",
+	"Sarah",
+	"Steffi",
+	"Tara",
+	"Tom",
+	"Yiping"
+];
